2156dafd218b9ea73947abcb45a3ca7387e1d47d
gnix22 <gjnix22@gmail.com>
2026-02-18 23:06:36 -0500

    got successful output for q4

--- CSCI_271_Garrett_Nix_Assignment_3_Character_Counter.java
+++ CSCI_271_Garrett_Nix_Assignment_3_Character_Counter.java
@@ -1,24 +1,37 @@
 class NumberCounter{
     // Possible avenues to try:
     // - set an array that puts each indivdual character in that array, recursively count through, matching to a search number.
+    private String stringIn_;
     private int[] numArray_;
     private int searchNum_;
-    private int searchNumCount_ = 0;
+    private int searchNumCount_;
     // initializer
-    public NumberCounter(int[] numArrayIn, int searchNumIn){
-        numArray_ = numArrayIn;
+    public NumberCounter(String strIn, int searchNumIn){
+        stringIn_ = strIn;
+        numArray_ = strToNumArray(strIn);
         searchNum_ = searchNumIn;
         searchNumCount_ = recurserCounter_(numArray_.length, 0);
     }
-
+    public void printResult(){
+        System.out.println("N= " + stringIn_);
+        System.out.println("D= " + searchNum_);
+        System.out.println("Answer= " + searchNumCount_);
+    }
+    private int[] strToNumArray(String s){
+        int[] convertedArray = new int[s.length()];
+        for(int i = 0; i < s.length(); i++){
+            char strElem = s.charAt(i);
+            convertedArray[i] = strElem - '0';
+        }
+        return convertedArray;
+    }
     private int recurserCounter_(int searchSize, int i) {
         if (searchSize == 0) {
             return 0;
         }
-
         if (searchNum_ == numArray_[i]) {
-            return 1 + recurserCounter_(searchSize - 1, i - 1);
+            return 1 + recurserCounter_(searchSize - 1, i + 1);
         }
-        return recurserCounter_(searchSize - 1, i - 1);
+        return recurserCounter_(searchSize - 1, i + 1);
     }
 }
