81196fb99f2481baf51d8a33d81771027ddd278f
gnix22 <gjnix22@gmail.com>
2026-02-18 22:31:28 -0500

    more work on methods

--- CSCI_271_Garrett_Nix_Assignment_3_Character_Counter.java
+++ CSCI_271_Garrett_Nix_Assignment_3_Character_Counter.java
@@ -3,16 +3,22 @@ class NumberCounter{
     // - set an array that puts each indivdual character in that array, recursively count through, matching to a search number.
     private int[] numArray_;
     private int searchNum_;
-    private int searchNumCount_;
+    private int searchNumCount_ = 0;
     // initializer
     public NumberCounter(int[] numArrayIn, int searchNumIn){
         numArray_ = numArrayIn;
         searchNum_ = searchNumIn;
+        searchNumCount_ = recurserCounter_(numArray_.length, 0);
     }
 
-    private int recurserCounter_(int searchSize){
-        if(searchSize > 0){
-            
+    private int recurserCounter_(int searchSize, int i) {
+        if (searchSize == 0) {
+            return 0;
         }
+
+        if (searchNum_ == numArray_[i]) {
+            return 1 + recurserCounter_(searchSize - 1, i - 1);
+        }
+        return recurserCounter_(searchSize - 1, i - 1);
     }
 }
