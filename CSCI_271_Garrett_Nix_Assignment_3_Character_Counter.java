fda8fb296954f97d3de34bc032fb198a17858fdd
gnix22 <gjnix22@gmail.com>
2026-02-18 21:05:36 -0500

    general set up for recursive idea

---
+++ CSCI_271_Garrett_Nix_Assignment_3_Character_Counter.java
@@ -0,0 +1,18 @@
+class NumberCounter{
+    // Possible avenues to try:
+    // - set an array that puts each indivdual character in that array, recursively count through, matching to a search number.
+    private int[] numArray_;
+    private int searchNum_;
+    private int searchNumCount_;
+    // initializer
+    public NumberCounter(int[] numArrayIn, int searchNumIn){
+        numArray_ = numArrayIn;
+        searchNum_ = searchNumIn;
+    }
+
+    private int recurserCounter_(int searchSize){
+        if(searchSize > 0){
+            
+        }
+    }
+}
---
+++ CSCI_271_Garrett_Nix_Assignment_3_Main.java
@@ -0,0 +1,10 @@
+public class CSCI_271_Garrett_Nix_Assignment_3_Main{
+    public static void main(String[] args){
+        try{
+            String numStringIn = args[0]; // first get string of numbers, used for parsing each character.
+            int searchNum = Integer.parseInt(args[1]);
+        } catch (ArrayIndexOutOfBoundsException e){
+            System.err.println("error: please provide a string of numbers followed by a number to search the string for.");
+        }
+    }
+}
