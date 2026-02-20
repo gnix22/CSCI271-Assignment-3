4e4af42e134d6db52b5d471a9918bbeba39d7116
gnix22 <gjnix22@gmail.com>
2026-02-20 16:49:45 -0500

    got some successful outputs

--- CSCI_271_Garrett_Nix_Assignment_3_Character_Counter.java
+++ CSCI_271_Garrett_Nix_Assignment_3_Character_Counter.java
@@ -4,23 +4,35 @@ class Counter{
     private String stringIn_;
     private long[] numArray_;
     private char[] charArray_;
+    private char[] charOutArray_;
+    private String stringOut_;
     private int searchNum_;
     private int searchNumCount_;
     private char searchChar_;
+    private boolean isInt_;
+    private long max_;
     // initializer
-    public Counter(String strIn, int searchNumIn){
+    public Counter(String strIn, int searchNumIn, boolean isInt){
         stringIn_ = strIn;
         charArray_ = stringIn_.toCharArray();
+        charOutArray_ = new char[stringIn_.length()];
         numArray_ = new long[stringIn_.length()];
         numArray_ = strToNumArray(0);
         searchNum_ = searchNumIn;
-        searchNumCount_ = recurserCounter_(numArray_.length-1, 0, true);
+        if(isInt){
+            isInt_ = true;
+        } else isInt_ = false;
+        searchNumCount_ = recurserCounter_(numArray_.length-1, 0);
+        stringOut_ = reverseOutput(numArray_.length - 1);
     }
     public Counter(String strIn, char searchCharIn){
+        isInt_ = false; // assume that if isInt isn't passed, it is false 
         stringIn_ = strIn;
         charArray_ = stringIn_.toCharArray();
+        charOutArray_ = new char[stringIn_.length()];
         searchChar_ = searchCharIn;
-        searchNumCount_ = recurserCounter_(charArray_.length-1, 0, false);
+        searchNumCount_ = recurserCounter_(charArray_.length-1, 0);
+        stringOut_ = reverseOutput(charArray_.length - 1);
     }
     public int lengthReader(int i){
         if(i < 0){
@@ -31,41 +43,74 @@ class Counter{
         }
         return lengthReader(i-1);
     }
-    public void printResult(int choice, boolean isInt){
-        if(isInt){
-            switch (choice - 2) {
+    public void printResult(int choice){
+        if(isInt_ && choice >= 4){
+            switch (choice - 3) {
                 case 1:
-                    System.out.println("the max of the string is " + max(numArray_.length));
+                    System.out.println("the max of the string is " + max_);
                     break;
                 case 2:
-                    System.out.println("the number of times " + searchNum_ + "happens is " + searchNumCount_ + "times");
-                default:
+                    System.out.println("the sum of even elements is ");
                     break;
+                default:
+                    throw new IllegalArgumentException("operations not available for characters");
             }
         }
+        switch (choice) {
+            case 1:
+                System.out.println("the length of the string is: " + lengthReader(stringIn_.length() - 1));
+                break;
+            case 2:
+                System.out.println("the number of times the search element occurs is " + searchNumCount_);
+                break;
+            case 3:
+                System.out.println("the backwards output of the string is: " + stringOut_);
+                break;
+            default:
+                break;
+        }
     }
-    public int recurserCounter_(int searchSize, int i, boolean isInt) {
+    public int recurserCounter_(int searchSize, int i) {
         if (searchSize < 0) {
             return 0;
         }
-        if (isInt && searchNum_ == numArray_[i]) { // shortcircuit logic error fix
-            isInt = true; // likely unneeded, but for insurance.
-            return 1 + recurserCounter_(searchSize - 1, i + 1, isInt);
-        } else if(searchChar_ == charArray_[i] && !isInt){
-            isInt = false; // likely unneeded, but for insurance.
-            return 1 + recurserCounter_(searchSize - 1, i + 1, isInt);
+        if (isInt_ && searchNum_ == numArray_[i]) { // shortcircuit logic error fix
+            isInt_ = true; // likely unneeded, but for insurance.
+            return 1 + recurserCounter_(searchSize - 1, i + 1);
+        } else if(searchChar_ == charArray_[i] && !isInt_){
+            isInt_ = false; // likely unneeded, but for insurance.
+            return 1 + recurserCounter_(searchSize - 1, i + 1);
         }
-        return recurserCounter_(searchSize - 1, i + 1, isInt);
+        return recurserCounter_(searchSize - 1, i + 1);
     }
     public long max(int i){
         if (i < 0){
             return 0;
         }
         if(numArray_[i-1] > max(i)){
-            long max = numArray_[i-1];
-            return max;
+            max_ = numArray_[i-1];
         }
         return max(i - 1);
+    }
+    public String reverseOutput(int i){
+        if(i < 0){
+            return new String(charOutArray_);
+        }
+        if(isInt_){
+            charOutArray_[i] = (char) ('0' + numArray_[numArray_.length-1-i]); // '0' to get back what we subtracted to get numArray.
+        } else {
+            charOutArray_[i] = charArray_[charArray_.length-1-i];
+        }
+        return reverseOutput(i-1);
+    }
+    public long sumEven(int i){
+        if(i <= 1){
+            return 0;
+        }
+        if(numArray_[i] % 2 == 0){
+            return numArray_[i] + sumEven(i-1);   
+        }
+        return sumEven(i - 1);
     }
     // HELPER FUNCTIONS
     private long[] strToNumArray(int i){
--- CSCI_271_Garrett_Nix_Assignment_3_Main.java
+++ CSCI_271_Garrett_Nix_Assignment_3_Main.java
@@ -1,22 +1,32 @@
+import java.util.Scanner;
 public class CSCI_271_Garrett_Nix_Assignment_3_Main{
+    
     public static void main(String[] args){
+        Scanner userIn = new Scanner(System.in);
         try{
             String stringIn = args[0];
             System.out.println("select an option:");
-            System.out.println("    character related options:");
-            System.out.println("        1: Count the number of elements in the string.");
-            System.out.println("        2: Count number of times search element occurs.");
+            System.out.println("    1: output the length of the input string");
+            System.out.println("    2: print the number of times the search element occurs");
+            System.out.println("    3: output the input string backwards");
+            System.out.println("    4: output the maximum of the list of numbers");
+            System.out.println("    5: sum all even elements, output the result");
+            int choice = userIn.nextInt();
+            System.out.println(args[0] + " " + args[1] + " " + " " + args[2]);
             if(args[2].charAt(0) == 'i'){
                 int searchNum = Integer.parseInt(args[1]);
-                Counter result = new Counter(stringIn, searchNum);
-                result.printResult();
+                Counter result = new Counter(stringIn, searchNum, true);
+                result.printResult(choice);
             } else if(args[2].charAt(0) == 'c'){
                 char searchChar = args[1].charAt(0);
                 Counter result = new Counter(stringIn, searchChar);
-                result.printResult();
+                result.printResult(choice);
             } else System.err.println("please ensure you provide either an int OR char argument.");
         } catch (ArrayIndexOutOfBoundsException e){
+            e.printStackTrace();
             System.err.println("error: please provide a string of numbers followed by a number to search the string for.");
+        } catch (IllegalArgumentException e){
+            System.err.println(e.getMessage());
         }
     }
 }
