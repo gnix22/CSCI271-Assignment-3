b4fb9c17fe50c4fba7b1ce2f3200c6667abb3d08
gnix22 <gjnix22@gmail.com>
2026-02-20 14:44:30 -0500

    got rid of little_cli due to time constraints, work on more questions answered + user input for method calls

--- CSCI_271_Garrett_Nix_Assignment_3_Character_Counter.java
+++ CSCI_271_Garrett_Nix_Assignment_3_Character_Counter.java
@@ -1,16 +1,26 @@
-class NumberCounter{
+class Counter{
     // Possible avenues to try:
     // - set an array that puts each indivdual character in that array, recursively count through, matching to a search number.
     private String stringIn_;
-    private int[] numArray_;
+    private long[] numArray_;
+    private char[] charArray_;
     private int searchNum_;
     private int searchNumCount_;
+    private char searchChar_;
     // initializer
-    public NumberCounter(String strIn, int searchNumIn){
+    public Counter(String strIn, int searchNumIn){
         stringIn_ = strIn;
-        numArray_ = strToNumArray(strIn);
+        charArray_ = stringIn_.toCharArray();
+        numArray_ = new long[stringIn_.length()];
+        numArray_ = strToNumArray(0);
         searchNum_ = searchNumIn;
-        searchNumCount_ = recurserCounter_(numArray_.length, 0);
+        searchNumCount_ = recurserCounter_(numArray_.length-1, 0, true);
+    }
+    public Counter(String strIn, char searchCharIn){
+        stringIn_ = strIn;
+        charArray_ = stringIn_.toCharArray();
+        searchChar_ = searchCharIn;
+        searchNumCount_ = recurserCounter_(charArray_.length-1, 0, false);
     }
     public int lengthReader(int i){
         if(i < 0){
@@ -21,26 +31,48 @@ class NumberCounter{
         }
         return lengthReader(i-1);
     }
-    public void printResult(){
-        System.out.println("N = " + stringIn_);
-        System.out.println("D = " + searchNum_);
-        System.out.println("Answer = " + searchNumCount_);
-    }
-    private int[] strToNumArray(String s){
-        int[] convertedArray = new int[s.length()];
-        for(int i = 0; i < s.length(); i++){
-            char strElem = s.charAt(i);
-            convertedArray[i] = strElem - '0';
+    public void printResult(int choice, boolean isInt){
+        if(isInt){
+            switch (choice - 2) {
+                case 1:
+                    System.out.println("the max of the string is " + max(numArray_.length));
+                    break;
+                case 2:
+                    System.out.println("the number of times " + searchNum_ + "happens is " + searchNumCount_ + "times");
+                default:
+                    break;
+            }
         }
-        return convertedArray;
     }
-    private int recurserCounter_(int searchSize, int i) {
+    public int recurserCounter_(int searchSize, int i, boolean isInt) {
         if (searchSize < 0) {
             return 0;
         }
-        if (searchNum_ == numArray_[i]) {
-            return 1 + recurserCounter_(searchSize - 1, i + 1);
+        if (isInt && searchNum_ == numArray_[i]) { // shortcircuit logic error fix
+            isInt = true; // likely unneeded, but for insurance.
+            return 1 + recurserCounter_(searchSize - 1, i + 1, isInt);
+        } else if(searchChar_ == charArray_[i] && !isInt){
+            isInt = false; // likely unneeded, but for insurance.
+            return 1 + recurserCounter_(searchSize - 1, i + 1, isInt);
         }
-        return recurserCounter_(searchSize - 1, i + 1);
+        return recurserCounter_(searchSize - 1, i + 1, isInt);
+    }
+    public long max(int i){
+        if (i < 0){
+            return 0;
+        }
+        if(numArray_[i-1] > max(i)){
+            long max = numArray_[i-1];
+            return max;
+        }
+        return max(i - 1);
+    }
+    // HELPER FUNCTIONS
+    private long[] strToNumArray(int i){
+        if (i >= stringIn_.length()){
+            return numArray_;
+        } 
+        numArray_[i] = charArray_[i] - '0';
+        return strToNumArray(i + 1);
     }
 }
--- CSCI_271_Garrett_Nix_Assignment_3_Main.java
+++ CSCI_271_Garrett_Nix_Assignment_3_Main.java
@@ -1,14 +1,20 @@
 public class CSCI_271_Garrett_Nix_Assignment_3_Main{
     public static void main(String[] args){
         try{
-            String numStringIn = args[0]; // first get string of numbers, used for parsing each character.
-            int searchNum = Integer.parseInt(args[1]);
-            System.out.println(numStringIn);
-            System.out.println(searchNum);
-            NumberCounter result = new NumberCounter(numStringIn, searchNum);
-            result.printResult();
-            LittleCLI cli = new LittleCLI(result);
-            cli.run();
+            String stringIn = args[0];
+            System.out.println("select an option:");
+            System.out.println("    character related options:");
+            System.out.println("        1: Count the number of elements in the string.");
+            System.out.println("        2: Count number of times search element occurs.");
+            if(args[2].charAt(0) == 'i'){
+                int searchNum = Integer.parseInt(args[1]);
+                Counter result = new Counter(stringIn, searchNum);
+                result.printResult();
+            } else if(args[2].charAt(0) == 'c'){
+                char searchChar = args[1].charAt(0);
+                Counter result = new Counter(stringIn, searchChar);
+                result.printResult();
+            } else System.err.println("please ensure you provide either an int OR char argument.");
         } catch (ArrayIndexOutOfBoundsException e){
             System.err.println("error: please provide a string of numbers followed by a number to search the string for.");
         }
